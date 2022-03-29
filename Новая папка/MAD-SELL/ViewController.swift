68fcd9fc47a487c85d1ec55cd99830c6296b8f74
Stasia <83281012+dekeyofspace@users.noreply.github.com>
2022-03-29 14:05:29 +0300

    Add files via upload

---
+++ Новая папка/MAD-SELL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD-SELL
+//
+//  Created by Student on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
